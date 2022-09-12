d0e15e811fef17bedea60375497667e219b227c5
Tural Guluev <tural-98@bk.ru>
2022-09-12 17:37:10 +0300

    finished the implementation PlayerController class

--- game/src/main/java/com/game/exceptions/BadRequestException.java
+++ game/src/main/java/com/game/exceptions/BadRequestException.java
@@ -2,7 +2,6 @@ package com.game.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import sun.jvm.hotspot.c1.Runtime1;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException{
